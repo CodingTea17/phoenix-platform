2a7ffddf98e8dc8a259d32ea04bcd75b91b23ac5
Dawson Mortenson <dawson.mortenson@gmail.com>
2018-05-28 23:10:34 -0700

    define and create a player api controller to route player CRUD and the accompanying view

--- lib/platform_web/router.ex
+++ lib/platform_web/router.ex
@@ -28,5 +28,6 @@ defmodule PlatformWeb.Router do
 
     resources "/games", GameController, except: [:new, :edit]
     resources "/gameplays", GameplayController, except: [:new, :edit]
+    resources "/payers", PlayerApiController, except: [:new, :edit]
   end
 end
